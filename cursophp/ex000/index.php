b19ae6b9a623be79e360a13bd8e4eeff40f83a18
Paula Beatriz <107193691+Paulabeatriz09@users.noreply.github.com>
2023-03-09 17:45:36 -0300

    Add files via upload

---
+++ cursophp/ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Primeiro site</title>
+</head>
+<body>
+    <h1>Primeiro site</h1>
+    <?php
+    echo"Olá mundo \u{1F30E}";
+    
+    $nome="Paula";
+    echo"$nome";
+    ?>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In tempora hic asperiores sed a ipsa molestias odio est quod aliquam rem, praesentium quaerat beatae cum, earum quae fugit quis amet.</p>
+    
+</body>
+</html>
